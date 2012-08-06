077bf5435a8a9fe5c67613a6e55b58151cc358fc
Mike Nowak <mikenowak@users.noreply.github.com>
2012-08-06 09:29:35 +0100

    first commit

---
+++ application/manifests/config.pp
@@ -0,0 +1,45 @@
+# == Class: boilerplate::config
+#
+# This class exists to coordinate all configuration related actions,
+# functionality and logical units in a central place.
+#
+#
+# === Parameters
+#
+# This class does not provide any parameters.
+#
+#
+# === Examples
+#
+# This class may be imported by other classes to use its functionality:
+#   class { 'boilerplate::config': }
+#
+# It is not intended to be used directly by external resources like node
+# definitions or other modules.
+#
+#
+# === Authors
+#
+# * John Doe <mailto:john.doe@example.com>
+#
+class boilerplate::config {
+
+  #### Configuration
+
+  # set params: in operation
+  if $boilerplate::ensure == 'present' {
+    $config_ensure = 'present'
+  # set params: removal
+  } else {
+    $config_ensure = 'absent'
+  }
+
+  # action
+  file { $boilerplate::params::config_path:
+    ensure  => $config_ensure,
+    mode    => $boilerplate::params::config_mode,
+    owner   => $boilerplate::params::config_owner,
+    group   => $boilerplate::params::config_group,
+    content => template($boilerplate::template),
+  }
+}
---
+++ service/manifests/params.pp
@@ -0,0 +1,101 @@
+# == Class: boilerplate::params
+#
+# This class exists to
+# 1. Declutter the default value assignment for class parameters.
+# 2. Manage internally used module variables in a central place.
+#
+# Therefore, many operating system dependent differences (names, paths, ...)
+# are addressed in here.
+#
+#
+# === Parameters
+#
+# This class does not provide any parameters.
+#
+#
+# === Examples
+#
+# This class is not intended to be used directly.
+#
+#
+# === Links
+#
+# * {Puppet Docs: Using Parameterized Classes}[http://j.mp/nVpyWY]
+#
+#
+# === Authors
+#
+# * John Doe <mailto:john.doe@example.com>
+#
+class boilerplate::params {
+
+  #### Default values for the parameters of the main module class, init.pp
+
+  # ensure
+  $ensure = 'present'
+
+  # autoupgrade
+  $autoupgrade = false
+
+  # service status
+  $status = 'enabled'
+
+  # template file
+  $template = 'FIXME/TODO'
+
+  # options
+  $options = ''
+
+  #### Internal module values
+
+  # packages
+  case $::operatingsystem {
+    'RedHat', 'CentOS', 'Fedora': {
+      $package = [ 'FIXME/TODO' ]
+    }
+    'Debian', 'Ubuntu': {
+      $package = [ 'FIXME/TODO' ]
+    }
+    default: {
+      fail("\"${module_name}\" provides no package default value for \"${::operatingsystem}\"")
+    }
+  }
+
+  # service parameters
+  case $::operatingsystem {
+    'RedHat', 'CentOS', 'Fedora': {
+      $service            = 'FIXME/TODO'
+      $service_hasrestart = true
+      $service_hasstatus  = true
+      $service_pattern    = $service
+    }
+    'Debian', 'Ubuntu': {
+      $service            = 'FIXME/TODO'
+      $service_hasrestart = true
+      $service_hasstatus  = true
+      $service_pattern    = $service
+    }
+    default: {
+      fail("\"${module_name}\" provides no service parameters for \"${::operatingsystem}\"")
+    }
+  }
+
+  # config parameters
+  case $::operatingsystem {
+    'RedHat', 'CentOS', 'Fedora': {
+      $config_path        = 'FIXME/TODO'
+      $config_mode        = '0600'
+      $config_owner       = 'root'
+      $config_group       = 'root'
+    }
+    'Debian', 'Ubuntu': {
+      $config_path        = 'FIXME/TODO'
+      $config_mode        = '0600'
+      $config_owner       = 'root'
+      $config_group       = 'root'
+    }
+    default: {
+      fail("\"${module_name}\" provides no config parameters for \"${::operatingsystem}\"")
+    }
+  }
+}
