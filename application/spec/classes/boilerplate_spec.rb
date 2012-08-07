03417b7b07e2d033a6002759926ba550c9e215d4
Mike Nowak <mikenowak@users.noreply.github.com>
2012-08-07 13:40:10 +0100

    Adding boilerplate rspec test framework

---
+++ application/spec/classes/boilerplate_spec.rb
@@ -0,0 +1,171 @@
+require 'spec_helper'
+
+describe 'boilerplate', :type => 'class' do
+
+  let(:title) { 'boilerplate' }
+  let(:node) { 'puppetdev.hiveworx.co.uk' }
+
+  debianish              = [ 'Debian', 'Ubuntu' ]
+  debianish_package      = '[FIXME/TODO]'
+  debianish_config_path  = '[FIXME/TODO]'
+  debianish_config_mode  = '0644'
+  debianish_config_owner = 'root'
+  debianish_config_group = 'root'
+
+  redhatish              = [ 'RedHat', 'CentOS', 'Fedora' ]
+  redhatish_package      = '[FIXME/TODO]'
+  redhatish_config_path  = '[FIXME/TODO]'
+  redhatish_config_mode  = '0644'
+  redhatish_config_owner = 'root'
+  redhatish_config_group = 'root'
+
+  unknown             = [ 'Foobar' ]
+
+
+  describe 'Test installation' do
+
+    debianish.each do |os|
+      describe "for operating system #{os}" do
+        let(:params) { {} }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { should contain_package("#{debianish_package}").with_ensure('present') }
+        it { should contain_file("#{debianish_config_path}").with(
+          'ensure' => 'present',
+          'mode'   => "#{debianish_config_mode}",
+          'owner'  => "#{debianish_config_owner}",
+          'group'  => "#{debianish_config_group}",
+        ) }
+        it 'should allow version to be overridden to a specific version number' do
+          params[:version] = '1.2.3'
+          subject.should contain_package("#{debianish_package}").with_ensure('1.2.3')
+        end
+        it 'should allow version to be overridden with to latest' do
+          params[:version] = 'latest'
+          subject.should contain_package("#{debianish_package}").with_ensure('latest')
+        end
+        it 'should allow template to be overridden' do
+          params[:template] = 'boilerplate/spec.erb'
+          content = catalogue.resource('file', "#{debianish_config_path}").send(:parameters)[:content]
+          content.should match "value_a"
+        end
+        it 'should generate a template that uses a custom option' do
+          params[:template] = 'boilerplate/spec.erb'
+          params[:options]  = { 'opt_a' => 'value_z' }
+          content = catalogue.resource('file', "#{debianish_config_path}").send(:parameters)[:content]
+          content.should match "value_z"
+        end
+      end
+    end
+
+    redhatish.each do |os|
+      describe "for operating system #{os}" do
+        let(:params) { {} }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { should contain_package("#{redhatish_package}").with_ensure('present') }
+        it { should contain_file("#{redhatish_config_path}").with(
+          'ensure' => 'present',
+          'mode'   => "#{redhatish_config_mode}",
+          'owner'  => "#{redhatish_config_owner}",
+          'group'  => "#{redhatish_config_group}",
+        ) }
+        it 'should allow version to be overridden to a specific version number' do
+          params[:version] = '1.2.3'
+          subject.should contain_package("#{redhatish_package}").with_ensure('1.2.3')
+        end
+        it 'should allow version to be overridden with to latest' do
+          params[:version] = 'latest'
+          subject.should contain_package("#{redhatish_package}").with_ensure('latest')
+        end
+        it 'should allow template to be overridden' do
+          params[:template] = 'boilerplate/spec.erb'
+          content = catalogue.resource('file', "#{redhatish_config_path}").send(:parameters)[:content]
+          content.should match "value_a"
+        end
+        it 'should generate a template that uses a custom option' do
+          params[:template] = 'boilerplate/spec.erb'
+          params[:options]  = { 'opt_a' => 'value_z' }
+          content = catalogue.resource('file', "#{redhatish_config_path}").send(:parameters)[:content]
+          content.should match "value_z"
+        end
+      end
+    end
+
+    unknown.each do |os|
+      describe "for an unknown operating system" do
+        let(:params) { {} }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { expect { should contain_package('boilerplate').with_ensure('present') }.to raise_error(Puppet::Error, /"#{title}" provides no package default value for "#{os}"/) }
+      end
+    end
+
+  end
+
+describe 'Test uninstallation' do
+
+    debianish.each do |os|
+      describe "for operating system #{os}" do
+        let(:params) { {:ensure => 'absent' } }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { should contain_package("#{debianish_package}").with_ensure('purged') }
+        it { should contain_file("#{debianish_config_path}").with_ensure('absent') }
+      end
+    end
+
+    redhatish.each do |os|
+
+      describe "for operating system #{os}" do
+        let(:params) { {:ensure => 'absent' } }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { should contain_package("#{redhatish_package}").with_ensure('purged') }
+        it { should contain_file("#{redhatish_config_path}").with_ensure('absent') }
+      end
+    end
+
+    unknown.each do |os|
+      describe "for an unknown operating system" do
+        let(:params) {{}}
+        let(:facts) { {:operatingsystem => os } }
+
+        it { expect { should contain_package('boilerplate').with_ensure('present') }.to raise_error(Puppet::Error, /"#{title}" provides no package default value for "#{os}"/) }
+      end
+    end
+
+  end
+
+  describe 'Test for invalid parameters' do
+
+    debianish.each do |os|
+      describe "for operating system #{os}" do
+        let(:params) { {:ensure => 'foo' } }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { expect { should contain_package("#{debianish_package}").with_ensure('present') }.to raise_error(Puppet::Error, /"#{params[:ensure]}" is not a valid ensure parameter value/) }
+      end
+    end
+
+    redhatish.each do |os|
+      describe "for operating system #{os}" do
+        let(:params) { {:ensure => 'foo' } }
+        let(:facts) { {:operatingsystem => os } }
+
+        it { expect { should contain_package("#{redhatish_package}").with_ensure('present') }.to raise_error(Puppet::Error, /"#{params[:ensure]}" is not a valid ensure parameter value/) }
+      end
+    end
+
+    unknown.each do |os|
+      describe "for an unknown operating system" do
+        let(:params) {{}}
+        let(:facts) { {:operatingsystem => os } }
+
+        it { expect { should contain_package('boilerplate').with_ensure('present') }.to raise_error(Puppet::Error, /"#{title}" provides no package default value for "#{os}"/) }
+      end
+    end
+
+  end
+
+end
